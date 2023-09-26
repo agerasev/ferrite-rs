cc0cac00b10840c0f66e1f3b384c726a97598bcc
Alexey Gerasev <alexey.gerasev@gmail.com>
2023-09-26 12:34:26 +0700

    Add more methods to AtomicVariable, remove Deref<Target=AsyncAtomic> to avoid accidental modification without notification

--- src/atomic.rs
+++ src/atomic.rs
@@ -5,12 +5,9 @@ use crate::{
 };
 use async_atomic::Atomic as AsyncAtomic;
 use futures::task::{waker_ref, ArcWake};
-use std::{
-    ops::Deref,
-    sync::{
-        atomic::{AtomicBool, Ordering},
-        Arc,
-    },
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc,
 };
 
 pub struct AtomicVariable<T: Type> {
@@ -53,27 +50,48 @@ impl<T: Type> AtomicVariable<T> {
             Stage::Committed => (),
         }
     }
+
+    pub fn load(&self) -> T {
+        self.value.load()
+    }
+
     pub fn store(self: &Arc<Self>, value: T) {
         self.value.store(value);
         self.update.store(true, Ordering::Release);
         self.notify(&mut self.variable.lock());
+    }
+    pub fn swap(self: &Arc<Self>, value: T) -> T {
+        let old = self.value.swap(value);
+        self.update.store(true, Ordering::Release);
+        self.notify(&mut self.variable.lock());
+        old
+    }
+    pub fn compare_exchange(self: &Arc<Self>, current: T, new: T) -> Result<T, T> {
+        let res = self.value.compare_exchange(current, new);
+        if res.is_ok() {
+            self.update.store(true, Ordering::Release);
+            self.notify(&mut self.variable.lock());
+        }
+        res
+    }
+    pub fn fetch_update<F: FnMut(T) -> Option<T>>(self: &Arc<Self>, f: F) -> Result<T, T> {
+        let res = self.value.fetch_update(f);
+        if res.is_ok() {
+            self.update.store(true, Ordering::Release);
+            self.notify(&mut self.variable.lock());
+        }
+        res
     }
 }
 
 impl<T: Type> ArcWake for AtomicVariable<T> {
     fn wake_by_ref(this: &Arc<Self>) {
         // Variable is already locked when waker is called.
-        let mut not_locked = unsafe { LockedVariable::without_lock(&this.variable) };
-        this.notify(&mut not_locked);
+        let mut locked = unsafe { LockedVariable::without_lock(&this.variable) };
+        this.notify(&mut locked);
     }
 }
 
-impl<T: Type> Deref for AtomicVariable<T> {
-    type Target = AsyncAtomic<T>;
-    fn deref(&self) -> &Self::Target {
-        &self.value
-    }
-}
 impl<T: Type> AsRef<AsyncAtomic<T>> for AtomicVariable<T> {
     fn as_ref(&self) -> &AsyncAtomic<T> {
         &self.value
